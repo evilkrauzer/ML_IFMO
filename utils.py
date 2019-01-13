9d623ff8871e86d64937928ab4754d3f0b865502
evilkrauzer <evilkrauzer@gmail.com>
2019-01-14 01:03:46 +0300

    1

--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@ import numpy as np
 
 
 def load_data():
-    data = np.loadtxt('../HAPT Data Set/Train/X_train.txt', delimiter=' ')
-    classes = np.loadtxt('../HAPT Data Set/Train/y_train.txt')
+    data = np.loadtxt('../../HAPT Data Set/Train/X_train.txt', delimiter=' ')
+    classes = np.loadtxt('../../HAPT Data Set/Train/y_train.txt')
     return data, classes
